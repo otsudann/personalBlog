eced9eb71ba1fe995e660d043ee5f8ad60b54485
Daniel Augusto <daniel.aug.souza@gmail.com>
2023-02-23 16:43:48 -0300

    Testing new routes

--- main.py
+++ main.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello World</p>"
 
+@app.route("/hello/<name>")
+def hello_name(name):
+    return f"Hello {name}!"
+
+@app.route("/blog/<int:postID>")
+def show_blog(postID):
+    return f"Blog number {postID}"
+
+@app.route("/rev/<float:revNO>")
+def revision(revNO):
+    return f"Revision number {revNO}"
+
 if __name__ == "__main__":
     app.run()
