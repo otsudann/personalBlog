2743a463447e7dee58d7540b36b3a1c4001de218
Daniel Augusto <daniel.aug.souza@gmail.com>
2023-02-23 17:30:51 -0300

    Testing request methods post+get

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route("/admin")
-def hello_admin():
-    return f"Hello Admin"
+@app.route("/success/<name>")
+def success(name):
+    return f"welcome {name}"
 
-@app.route("/guest/<guest>")
-def hello_guest(guest):
-    return f"Hello {guest} as Guest!"
-
-@app.route("/user/<name>")
-def hello_user(name):
-    if name == "admin":
-        return redirect(url_for("hello_admin"))
+@app.route("/login", methods = ["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("success", name = user))
     else:
-        return redirect(url_for("hello_guest", guest = name))
+        user = request.args.get["nm"]
+        return redirect(url_for("success", name = user))
 
 if __name__ == "__main__":
     app.run()
